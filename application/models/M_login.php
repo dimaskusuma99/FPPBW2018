b00d4ab7c06b7ef09c19b185b4cdd1422e323afb
hansjulioo <37090339+hansjulioo@users.noreply.github.com>
2018-05-17 10:39:42 +0700

    perbaikan enkripsi password dan database

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -1,8 +1,8 @@
 <?php
 class M_login extends CI_Model{
     function cekadmin($username,$password){
-        $hasil=$this->db->query("SELECT * FROM tbl_pengguna WHERE pengguna_username='$username' AND pengguna_password=md5('$password')");
+        $hasil=$this->db->query("SELECT * FROM tbl_pengguna WHERE pengguna_username='$username' AND pengguna_password=sha1(md5($password))");
         return $hasil;
     }
-  
+
 }
